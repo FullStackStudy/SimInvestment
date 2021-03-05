82c87b2d3a2ed849d62e1a86429ade90b289206a
이영재 <youngjewall@me.com>
2021-03-05 16:04:32 +0900

    Create Project

---
+++ src/main/java/FSS/SimInvestment/SimInvestmentApplication.java
@@ -0,0 +1,13 @@
+package FSS.SimInvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimInvestmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FSS/SimInvestment/SimInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package FSS.SimInvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
