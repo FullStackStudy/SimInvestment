6889caea1b71395a713089b5b927368ed2e10f36
이영재 <youngjewall@me.com>
2021-03-08 23:48:45 +0900

    DB & MemberService connect

--- src/main/java/FSS/SimInvestment/controller/MemberController.java
+++ src/main/java/FSS/SimInvestment/controller/MemberController.java
@@ -1,6 +1,8 @@
 package FSS.SimInvestment.controller;
 
-import FSS.SimInvestment.User;
+import FSS.SimInvestment.domain.Member;
+import FSS.SimInvestment.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,22 +10,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public String login(Model model){
-        model.addAttribute("user", new User());
+        model.addAttribute("member", new Member());
         return "login";
     }
 
     @PostMapping("/login")
-    public String result(@ModelAttribute User user, Model model){
-        model.addAttribute("user", user);
+    public String result(@ModelAttribute Member member, Model model){
+        memberService.join(member);
+        model.addAttribute(member);
         return "result";
     }
-  
-    public String clickedLoginButton()
-    {
 
-    }
 }
