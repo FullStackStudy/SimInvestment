71c1083c5e69996ad58bbf785e6b7ec6f87e5026
hyeonQyu <dhk0561@naver.com>
2021-03-15 23:29:31 +0900

    [feat] 버튼 컴포넌트 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import Button from './components/Button';
 
 function App() {
-  return (
-      <div>Stock</div>
-  );
+    return (
+        <>
+            <Button color="gray" size="large">
+                큰 회색 버튼
+            </Button>
+            <Button color="red">빨간 버튼</Button>
+            <Button color="blue" size="small">
+                작은 파란 버튼
+            </Button>
+        </>
+    );
 }
 
 export default App;
