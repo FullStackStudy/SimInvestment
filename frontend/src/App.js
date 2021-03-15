240730e2b381029a4fa7cd8ebac3398ecc7679a7
hyeonQyu <dhk0561@naver.com>
2021-03-15 23:32:55 +0900

    [etc] 클릭 이벤트 예시

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,9 @@ function App() {
             <Button color="gray" size="large">
                 큰 회색 버튼
             </Button>
-            <Button color="red">빨간 버튼</Button>
+            <Button onClick={() => alert('빨간 버튼 클릭됨!')} color="red">
+                빨간 버튼
+            </Button>
             <Button color="blue" size="small">
                 작은 파란 버튼
             </Button>
