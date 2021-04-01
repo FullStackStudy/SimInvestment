ded6d91b3fef5f589b8ce38e4f992d728bc11e99
hyeonQyu <dhk0561@naver.com>
2021-04-01 23:12:02 +0900

    [feat] 회원가입 페이지 이동 링크

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import Button from './components/Button';
-import Input from './components/Input';
+import { Route, Link, Switch } from 'react-router-dom';
+import SignUp from './pages/SignUp';
 
 function App() {
     return (
         <>
-            <Button color="gray" size="large">
-                큰 회색 버튼
-            </Button>
-            <Button onClick={() => alert('빨간 버튼 클릭됨!')} color="red">
-                빨간 버튼
-            </Button>
-            <Button color="blue" size="small">
-                작은 파란 버튼
-            </Button>
-            <form>
-                <Input type="text" placeholder="내용을 입력하세요." size="large" />
-                <Input type="text" placeholder="내용을 입력하세요." size="medium" />
-                <Input type="text" placeholder="내용을 입력하세요." size="small" />
-            </form>
+            <Link to="/sign-up">회원가입</Link>
+            <Switch>
+                <Route path="/sign-up" component={SignUp} />
+            </Switch>
         </>
     );
 }
