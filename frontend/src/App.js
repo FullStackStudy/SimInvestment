b8d8b5ae2b287a6dc5ebebf9019ac857eec3a433
naeunchan <eunchan0492@naver.com>
2021-04-09 17:38:03 +0900

    index page 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import { Route, Link, Switch } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import AlertModal from './components/AlertModal';
+import Home from './pages/Home';
 
 function App() {
     return (
-        <>
-            <Link to="/sign-up">회원가입</Link>
-            <Switch>
-                <Route path="/sign-up" component={SignUp} />
-            </Switch>
+        <div>
+            {/*<Link to="/sign-up">회원가입</Link>*/}
+            {/*<Switch>*/}
+            {/*    <Route path="/sign-up" component={SignUp} />*/}
+            {/*</Switch>*/}
+            {/*<AlertModal />*/}
+            {/*<Home />*/}
+            <Route exact path="/" component={Home} />
+            <Route path="/sign-up" component={SignUp} />
             <AlertModal />
-        </>
+        </div>
     );
 }
 
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,29 @@
+import { Route, Link, Switch } from 'react-router-dom';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import AlertModal from '../components/AlertModal';
+import './scss/Home.scss';
+
+function Home() {
+    return (
+        <div className="Home" id="wrap">
+            <div id="header">logo or image</div>
+            <div className="box" id="container">
+                <form>
+                    <div className="box" id="login_form">
+                        <Input wide type="text" placeholder="아이디" />
+                        <Input wide type="password" placeholder="비밀번호" />
+                        <Button wide className="button" type="submit" color="blue">
+                            로그인
+                        </Button>
+                    </div>
+                    <div className="box" id="join">
+                        처음이신가요? <Link to="/sign-up">회원가입</Link>을 통해 저희와 함께하세요
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
