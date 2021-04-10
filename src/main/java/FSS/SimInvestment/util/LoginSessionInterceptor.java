cd566c735eead5e030ba50dd856a97e2579f17ce
이영재 <youngjewall@me.com>
2021-04-10 20:01:55 +0900

    [feat] Login Session Interceptor 등록

---
+++ src/main/java/FSS/SimInvestment/util/LoginSessionInterceptor.java
@@ -0,0 +1,37 @@
+package FSS.SimInvestment.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class LoginSessionInterceptor implements HandlerInterceptor {
+    public List loginEssential
+            = Arrays.asList("/member/**");
+
+    public List loginInessential
+            = Arrays.asList("/th/**");
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Long loginKey = (Long)request.getSession().getAttribute("loginKey");
+
+        if(loginKey != null){
+            request.getSession().setMaxInactiveInterval(18000); // 30min timeout
+            return true;}
+
+        else{
+            String destUri = request.getRequestURI();
+            String destQuery = request.getQueryString();
+            String dest = (destQuery == null) ? destUri : destUri+"?"+destQuery;
+            request.getSession().setAttribute("dest", dest);
+
+            response.sendRedirect("/th");
+            return false;
+        }
+    }
+}
