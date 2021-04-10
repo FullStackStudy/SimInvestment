7d44d4e7f5d0b29f868fac98650e92a39d8c9408
naeunchan <eunchan0492@naver.com>
2021-04-10 20:40:13 +0900

    login page redirection

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,7 @@
 import { Route, Link, Switch } from 'react-router-dom';
 import Button from '../components/Button';
 import Input from '../components/Input';
-import './scss/Home.scss';
+import './scss/Login.scss';
 
 function Login() {
     return (
